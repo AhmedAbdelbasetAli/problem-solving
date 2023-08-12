35ce74e9327de8c1852de1d2750957506eb512e6
Ahmed Ali <102226628+AhmedAbdelbasetAli@users.noreply.github.com>
2023-08-12 10:37:01 +0200

    Create Container With Most Water.js

---
+++ Container With Most Water.js
@@ -0,0 +1,28 @@
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ var maxArea = function(height) {
+    let max = 0;
+    let left = 0 ;
+    let right = height.length -1;
+
+    while(left<right){
+        let width = right - left;
+        let area = Math.min(height[left],height[right])*width
+        
+        max = Math.max(max,area);
+
+        if(height[left]<=height[right]){
+            left ++;
+        }else{
+            right --;
+        }
+
+    }
+    return max;
+};

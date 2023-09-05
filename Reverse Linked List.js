ea54b9f7be70e8066157bb0fb4d243c5ef430271
Ahmed Ali <102226628+AhmedAbdelbasetAli@users.noreply.github.com>
2023-09-05 09:51:08 +0200

    Create  Reverse Linked List.js

---
+++ Reverse Linked List.js
@@ -0,0 +1,12 @@
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+  var reverseList = function(head) {
+    if (!head || !head.next) {
+        return head;
+    }
+
+    const reversedHead = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    
+    return reversedHead;
+};

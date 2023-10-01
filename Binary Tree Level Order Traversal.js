463473f5a96a82349f3896316fe927fc53833e3a
Ahmed Ali <102226628+AhmedAbdelbasetAli@users.noreply.github.com>
2023-10-01 22:16:35 +0200

    Create Binary Tree Level Order Traversal.js

---
+++ Binary Tree Level Order Traversal.js
@@ -0,0 +1,40 @@
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level). 
+
+class TreeNode {
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+function levelOrder(root) {
+    if (!root) {
+        return [];
+    }
+
+    const result = [];
+    const queue = [root];
+
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+        const currentLevel = [];
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+            currentLevel.push(node.val);
+
+            if (node.left) {
+                queue.push(node.left);
+            }
+
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+
+        result.push(currentLevel);
+    }
+
+    return result;
+}

fc5a925f39b71de7647af925f104e653f95c1ea0
Ahmed Ali <102226628+AhmedAbdelbasetAli@users.noreply.github.com>
2023-10-24 20:05:58 +0200

    Create  Add Two Numbers.js

---
+++ Add Two Numbers.js
@@ -0,0 +1,33 @@
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+function addTwoNumbers(l1, l2) {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+    let carry = 0;
+
+    while (l1 || l2) {
+        const x = l1 ? l1.val : 0;
+        const y = l2 ? l2.val : 0;
+        const sum = x + y + carry;
+
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+
+    return dummyHead.next;
+}
